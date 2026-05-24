4adaf0637757ef86c2cca76fd2b3f2b87a3204e3
akilaisuru <akilaisurukahingala@gmail.com>
2026-05-25 00:47:49 +0530

    implement userModel and setup Clodinary

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { model } from "mongoose";
+
+export enum UserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  roles: UserRole[];
+}
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: [String],
+      enum: Object.values(UserRole),
+      default: [UserRole.USER],
+    },
+  },
+  { timestamps: true },
+);
+const Usermodel = model<IUser>("User", userSchema);
+export default Usermodel;
